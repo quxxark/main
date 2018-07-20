e10ec85aeb16fbb08206e53df2fe42b37c7a06d0
quxxark <quxxark@gmail.com>
2018-07-20 18:14:27 +0300

    minor changes

--- looog.py
+++ looog.py
@@ -3,7 +3,7 @@ import requests
 import getpass
 
 link = 'https://adc.luxoft.com/confluence/login.action'
-desination = 'https://adc.luxoft.com/confluence/display/HNAVSTD/Home'
+destination = 'https://adc.luxoft.com/confluence/display/HNAVSTD/Home'
 LOGIN = 'dkicha'
 PASSWORD = getpass.getpass('Enter your password: ')
 
@@ -11,6 +11,6 @@ with requests.Session() as session:
     session.get(link)
     login_data = dict(os_username=LOGIN, os_password=PASSWORD, login='Log in')
     session.post(link, data=login_data)
-    response = session.get(desination)
+    response = session.get(destination)
 
     print(response.status_code)
