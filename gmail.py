7ab87657a80281388c2c05d5b4de324871b26e07
quxxark <quxxark@gmail.com>
2018-07-29 17:09:40 +0300

    gmail script

---
+++ gmail.py
@@ -0,0 +1,98 @@
+from __future__ import print_function
+from apiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+import base64
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import mimetypes
+import os
+from apiclient import errors
+
+"""
+Для правильной работы скрипта необходимо:
+1. Наличие credentials.json:
+https://developers.google.com/gmail/api/quickstart/python?hl=ru
+Либо прописан конкретный путь, по которому .json лежит
+2. Быть в PATH директории
+"""
+
+class SendEmail:
+    def __init__(self, service):
+        self.service = service
+
+    def CreateMessage(self, sender, to, subject, message_text):
+        message = MIMEText(message_text)
+        message['to'] = to
+        message['from'] = sender
+        message['subject'] = subject
+        return {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
+
+    def CreateMessageWithAttachment(self, sender, to, subject, message_text, file_dir, filename):
+
+        message = MIMEMultipart()
+        message['to'] = to
+        message['from'] = sender
+        message['subject'] = subject
+
+        msg = MIMEText(message_text)
+        message.attach(msg)
+
+        path = os.path.join(file_dir, filename)
+        content_type, encoding = mimetypes.guess_type(path)
+
+        if content_type is None or encoding is not None:
+            content_type = 'application/octet-stream'
+        main_type, sub_type = content_type.split('/', 1)
+        if main_type == 'text':
+            fp = open(path, 'rb')
+            msg = MIMEText(fp.read(), _subtype=sub_type)
+            fp.close()
+        elif main_type == 'image':
+            fp = open(path, 'rb')
+            msg = MIMEImage(fp.read(), _subtype=sub_type)
+            fp.close()
+        elif main_type == 'audio':
+            fp = open(path, 'rb')
+            msg = MIMEAudio(fp.read(), _subtype=sub_type)
+            fp.close()
+        else:
+            fp = open(path, 'rb')
+            msg = MIMEBase(main_type, sub_type)
+            msg.set_payload(fp.read())
+            fp.close()
+
+        msg.add_header('Content-Disposition', 'attachment', filename=filename)
+        message.attach(msg)
+
+        return {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
+
+    def SendMessage(self, user_id, message):
+        try:
+            message = (self.service.users().messages().send(userId=user_id, body=message).execute())
+            print('Message Id: %s' % message['id'])
+            return message
+        except errors.HttpError as error:
+            print('An error occurred: %s' % error)
+
+
+SCOPES = 'https://mail.google.com/'
+store = file.Storage('token.json')
+creds = store.get()
+
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+
+service = build('gmail', 'v1', http=creds.authorize(Http()))
+sendInst = SendEmail(service)
+
+to_address = input('Recipient address: ')
+subject = input('Subject: ')
+message_text = input('Message body: ')
+
+message = sendInst.CreateMessage('me', to_address, subject, message_text)
+sendInst.SendMessage('me', message)
